a482be9fbfc02802e8f41f33612e9ed7ebab8302
Bruno Camargos <brunocamargosn@gmail.com>
2020-12-28 21:21:10 -0300

    creating database

---
+++ app/src/main/java/com/example/todoapp/data/ToDoDao.kt
@@ -0,0 +1,17 @@
+package com.example.todoapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.todoapp.data.model.ToDoData
+
+@Dao
+interface ToDoDao {
+    @Query(value = "SELECT * FROM TODO_TABLE ORDER BY id ASC")
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(toDoData: ToDoData)
+}
